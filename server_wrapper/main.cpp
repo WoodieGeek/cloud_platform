d51fb4835ab38278d459dcc8fd72b6dad4d36b72
woodiegeek <shakirov-ernest@mail.ru>
2020-02-12 01:30:16 +0300

    fix build dir:

--- server_wrapper/main.cpp
+++ server_wrapper/main.cpp
@@ -3,14 +3,15 @@
 #include <bits/stdc++.h>
 
 int main() {
-  try
-  {
+  try {
       TServer server("127.0.0.1", "8080");
+      server.AddHandler("/index", [](TRequest request) {
+              std::cout << request.Uri_ << "\n";
+              return "INDEX";
+      });
       std::cout << "Starting...\n";
       server.Run();
-  }
-  catch (std::exception& e)
-  {
+  } catch (std::exception& e) {
       std::cerr << "Exception: " << e.what() << "\n";
   }
   return 0;
